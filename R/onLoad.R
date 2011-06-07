9acc03474018b39dd7deb60725e8113d93a6c53e
Natalja Kurbatova <natalja@ebi.ac.uk>
2011-06-07 12:11:01 +0000

    new version
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ontoCAT@56017 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/onLoad.R
+++ R/onLoad.R
@@ -9,6 +9,11 @@ getOntology <- function(pathToURI) {
     new("Ontology", ontology=ontologyFromURI)
 }
 
+getOntologyNoReasoning <- function(pathToURI) {
+    ontologyFromURI<-.jnew("uk.ac.ebi.ontocat.utils.OntologyParser",pathToURI,"false")
+    new("Ontology", ontology=ontologyFromURI)
+}
+
 getEFO <- function() {
     ontologyEFO<-.jnew("uk.ac.ebi.ontocat.utils.OntologyParser")
     new("Ontology", ontology=ontologyEFO)
